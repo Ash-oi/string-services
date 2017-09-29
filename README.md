98bd9f5458ca8bab0df39956b1da407b6148e76c
AshleyMcVeigh <ashleymcveigh92@gmail.com>
2017-09-29 15:33:57 +1000

    added a few more functions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #string-services
+
 use `pug index.pug -w -P --out ./public/` to compile the pug files to html inside the "public" folder
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,27 @@
-function splitString(){
+function splitString() {
   let body = document.getElementById('split-on-body').value;
   let character = document.getElementById('split-on-character').value;
-  let str = '';
-  body.split(character).map(param => str += param+'\n')
-  document.getElementById('split-on-body').value = str;
+  if (character.length === 0) {
+    return false;
+  } else {
+    let str = '';
+    body.split(character).map(param => str += param+'\n')
+    document.getElementById('split-on-body').value = str;
+  }
 }
 
 function urlDecode() {
   document.getElementById('split-on-body').value = decodeURIComponent(document.getElementById('split-on-body').value);
+}
+
+function urlEncode() {
+  document.getElementById('split-on-body').value = encodeURIComponent(document.getElementById('split-on-body').value);
+}
+
+function base64Encode() {
+  document.getElementById('split-on-body').value = btoa(document.getElementById('split-on-body').value);
+}
+
+function base64Decode() {
+  document.getElementById('split-on-body').value = atob(document.getElementById('split-on-body').value);
 }
