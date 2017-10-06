066308dcf543d74d75057804d279a86eca8f5aef
AshleyMcVeigh <ashleymcveigh92@gmail.com>
2017-10-06 11:19:57 +1000

    fixed the silly overengineered regex code.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,70 +1,100 @@
 function splitString() {
-  let body = document.getElementById('split-on-body').value;
+  let body = document.getElementById('input-body').value;
   let character = document.getElementById('split-on-character').value;
+  let isRegex = document.getElementById("split-regex").checked;
+  let str = '';
+
   if (character.length === 0) {
     return false;
+  } else if (isRegex) {
+    try {
+      let splitRegex = new RegExp(character, 'g');
+      body.split(splitRegex).map(param => str += param+'\n')
+      document.getElementById('input-body').value = str;
+    } catch (error) {
+      alert("Not valid regex, or something else, who knows. ")
+    }
   } else {
-    let str = '';
     body.split(character).map(param => str += param+'\n')
-    document.getElementById('split-on-body').value = str;
+    document.getElementById('input-body').value = str;
   }
 }
 
 function urlDecode() {
-  document.getElementById('split-on-body').value = decodeURIComponent(document.getElementById('split-on-body').value);
+  document.getElementById('input-body').value = decodeURIComponent(document.getElementById('input-body').value);
 }
 
 function urlEncode() {
-  document.getElementById('split-on-body').value = encodeURIComponent(document.getElementById('split-on-body').value);
+  document.getElementById('input-body').value = encodeURIComponent(document.getElementById('input-body').value);
 }
 
 function base64Encode() {
-  document.getElementById('split-on-body').value = btoa(document.getElementById('split-on-body').value);
+  document.getElementById('input-body').value = btoa(document.getElementById('input-body').value);
 }
 
 function base64Decode() {
-  document.getElementById('split-on-body').value = atob(document.getElementById('split-on-body').value);
+  document.getElementById('input-body').value = atob(document.getElementById('input-body').value);
 }
 
+function isThisRegex(string) {
+  try {
+    let test = new RegExp(string)
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+
 function findAndReplace() {
-  let body = document.getElementById('split-on-body').value;
+  let body = document.getElementById('input-body').value;
   let character = document.getElementById('replace-on-character').value;
   let withCharacter = document.getElementById('replace-with-character').value;
+  let isRegex = document.getElementById("replace-regex").checked;
 
   if (character.length === 0) {
     return false;
   } else {
-    if (character.charAt(0) === '/') { // regex
+    if (isRegex) { // regex
       try {
-        let last = character.charAt(character.length - 1)
-        console.log(last)
-        let cleanedRegex = character.replace(/\//g, '');
-        console.log(cleanedRegex)
-        if (last == '/') {
-          let replaceRegex = new RegExp(cleanedRegex);
-          console.log(replaceRegex);
-          document.getElementById('split-on-body').value = body.replace(replaceRegex, withCharacter);
-        } else {
-          cleanedRegex = cleanedRegex.substring(0, cleanedRegex.length - 1);
-          let replaceRegex = new RegExp(cleanedRegex, last);
-          document.getElementById('split-on-body').value = body.replace(replaceRegex, withCharacter);
-        }
+        let replaceRegex = new RegExp(character, 'g');
+        document.getElementById('input-body').value = body.replace(replaceRegex, withCharacter);
       } catch (error) {
         alert("Not valid regex, or something else, who knows. ")
       }
     } else { // not regex
       let replaceRegex = new RegExp(character, 'g');
-      document.getElementById('split-on-body').value = body.replace(replaceRegex, withCharacter);
+      document.getElementById('input-body').value = body.replace(replaceRegex, withCharacter);
     }  
   }
 }
 
 function jsonFormat() {
-  let body = document.getElementById('split-on-body').value
+  let body = document.getElementById('input-body').value
   try {
     let parsedBody = JSON.parse(body)
-    document.getElementById('split-on-body').value = JSON.stringify(parsedBody, null, 2)
+    document.getElementById('input-body').value = JSON.stringify(parsedBody, null, 2)
   } catch (err) {
     alert("Invalid JSON (need quotes around keys etc.)");
   }
+}
+
+//https://api.github.com/repos/AshleyMcVeigh/string-services/commits
+
+function loadLastCommit() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.github.com/repos/AshleyMcVeigh/string-services/commits');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      const parsed = JSON.parse(xhr.response)
+      let date = new Date(parsed[0].commit.committer.date)
+      let dateDisplay = document.getElementById('lastUpdate')
+      dateDisplay.innerText = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+      dateDisplay.href = parsed[0].html_url;
+    }
+    else {
+      alert('Request failed.  Returned status of ' + xhr.status);
+    }
+  };
+  xhr.send();
 }
